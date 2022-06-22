a3d842ce2d9ddebb3f6bb3db5a74dd0cf3b2a3bb
sadettin uysal <tubasariz@gmail.com>
2022-06-21 21:11:00 -0400

    my first jenkins project

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            System.out.println("hello!  am I a cat?");
+        }
+    }
+}
